22a7f18e499997b2d451a1a5a0df31e4b2a066c6
Nitay <trakktttor@gmail.com>
2023-04-04 20:02:15 +0700

    Завершит подготовку к защите

---
+++ js/filters.js
@@ -0,0 +1,33 @@
+import { sortDiscussed, getRandomArray } from './utils.js';
+
+const filterForm = document.querySelector('.img-filters');
+const COUNT_RANDOM_PHOTO = 10;
+
+const toggleFilter = (choosen) => {
+  const currentFilter = document.querySelector('.img-filters__button--active');
+  currentFilter.classList.remove('img-filters__button--active');
+  choosen.classList.add('img-filters__button--active');
+};
+
+const setFilterClick = (data, cb) => {
+  filterForm.addEventListener('click', (evt) => {
+    switch (evt.target.id) {
+      case 'filter-random':
+        toggleFilter(evt.target);
+        cb(getRandomArray(data, COUNT_RANDOM_PHOTO));
+        break;
+
+      case 'filter-discussed':
+        toggleFilter(evt.target);
+        cb(sortDiscussed(data));
+        break;
+
+      case 'filter-default':
+        toggleFilter(evt.target);
+        cb(data);
+        break;
+    }
+  });
+};
+
+export { setFilterClick };
